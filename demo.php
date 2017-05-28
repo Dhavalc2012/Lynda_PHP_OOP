dbab0e5493e44656f65226f95b82534cde797af2
Dhaval <dhavalchheda@ymail.com>
2017-05-28 21:36:42 +0530

    Creation of the database class. Using __clone() construct to restrict the Database to only one instance since we need only one instance. We have created a static variable which stores database instance and we are using static method getConnection to create instance of our db.

--- demo.php
+++ demo.php
@@ -1,61 +1,24 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of demo
- *
- * @author dhava
- */
 require 'class.Address.inc';
-echo '<h2> Instantiing Address Class</h2>';
+require 'class.Database.inc';
+
+echo '<h2>Instantiating Address</h2>';
 $address = new Address;
-var_dump($address) ;
-echo   '<h2> Setting properties </h2>';
-$address->street_address_1 = '201 Baker Street';
-$address->city = 'London';
+
+echo '<h2>Setting properties...</h2>';
+$address->street_address_1 = '555 Fake Street';
+$address->city = 'Townsville';
 $address->subdivision_name = 'State';
-$address->postal_code = '12233';
-$address->country_name = 'UK';
+$address->country_name = 'United States of America';
 $address->address_type_id = 1;
-var_dump($address);
+echo $address;
 
-
-//Displaying Aaddress
-echo '<h2>Displaying the Address</h2>';
-echo $address->display();
-
-echo '<h2> Testing the magic method __get and __set';
-unset($address->postal_code);
-$address->postal_code = '';
-echo $address->display();
-
-
-echo '<h2> Testing __construct with the Address </h2>';
-$address2 = new Address([
-    'street_address_1' => '123 LinkedIn Avenue NY',
-    'city' => 'Mew York',
-    'subdivision_name' => 'State',
-    'postal_code' => '999',
-    'country_name' => 'Canada'
-]);
-
-echo $address2->display();
-
-echo '<h2> Testing of the __toString Method </h2>';
-
-echo $address2;
-
-echo '<h2> Address types using static property<h2>';
-var_dump(Address::$valid_address_types);
-
-echo '<h2> Testing the address type ID validation </h2>';
-for ($id = 0; $id <= 4 ; $id++) {
-    echo "<div> $id :";
-    echo Address::isValidAddressTypeId($id)?"Valid" : "Invalid";
-    echo "</div>";
-}
+echo '<h2>Testing Address __construct with an array</h2>';
+$address_2 = new Address(array(
+  'street_address_1' => '123 Phony Ave',
+  'city' => 'Villageland',
+  'subdivision_name' => 'Region',
+  'country_name' => 'Canada',
+));
+echo $address_2;
