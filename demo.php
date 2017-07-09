4b2e9972d27d8cd13c84776f98f8cf115d4406a8
Dhaval <dhavalchheda@ymail.com>
2017-07-09 16:55:13 +0530

    Inheritence in PHP. This example is to show how we can inherit from the BASE class to the CHILD class and also how the AutoLoad function works.

--- demo.php
+++ demo.php
@@ -1,18 +1,22 @@
 <?php
 
-require 'class.Address.inc';
-require 'class.Database.inc';
+/*
+ * * Define Autoloader string.className
+ */
+function __autoload($class_name) {
+    include 'class.'.$class_name .'.inc';
+}
 
-echo '<h2>Instantiating Address</h2>';
-$address = new Address;
+echo '<h2>Instantiating AddressResidence</h2>';
+$address_residence = new AddressResidence();
 
 echo '<h2>Setting properties...</h2>';
-$address->street_address_1 = '555 Fake Street';
-$address->city = 'Townsville';
-$address->subdivision_name = 'State';
-$address->country_name = 'United States of America';
-$address->address_type_id = 1;
-echo $address;
+$address_residence->street_address_1 = '555 Fake Street';
+$address_residence->city = 'Townsville';
+$address_residence->subdivision_name = 'State';
+$address_residence->country_name = 'United States of America';
+$address_residence->address_type_id = 1;
+echo $address_residence;
 
 echo '<h2>Testing Address __construct with an array</h2>';
 $address_2 = new Address(array(
